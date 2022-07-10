4f5fc140236758fef154501023be12e09ad0a680
Aether Kerstens <philippekerstens@gmail.com>
2022-07-10 00:26:06 -0700

    Added methods and explanations

--- arrays.rb
+++ arrays.rb
@@ -2,10 +2,22 @@
 anime_main_characters = ["Aoba Seragaki", "Izuku Midoriya", "Ash Ketchum", "Ciel Phantomhive", "Tanya von Degurechaff", "Naruto Uzumaki", "Boruto Uzumaki", "Luffy D Monkey"]
 
 # array of at least 4 integers
-my_favorite_numbers = [1, 3, 7, 9, 15, 21, 5, 0, 8]
+my_favorite_numbers = [1, 3, 7, 9, 15, 21, 5, 0, 8, 2]
 
 # array of at least 4 floats
-important_numbers = [3.14, 2.71, 1.61, 6.62, 6.02, 6.67, 1.38]
+important_numbers = [2.71, 1.61, 6.62, 6.02, 6.67, 1.38]
 
 # array of at least 4 booleans
 piercings = [true, false, true, false, false, true, true, false]
+
+# method.push - this method will add legosi lupus to the end of the list of anime_main_characters
+anime_main_characters.push "Legosi Lupus"
+
+# method.pop - this method will delete the last element in the list of my_favorite_numbers
+my_favorite_numbers.pop
+
+# method.shift - this method will delete the first element in the booleans list of piercings
+piercings.shift
+
+# method.unshift - this method will add 3.14 to the front list of important_numbers
+important_numbers.unshift 3.14
