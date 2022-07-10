e4d4f066f9736a723459f82894f295879bbe558d
Aether Kerstens <philippekerstens@gmail.com>
2022-07-09 22:46:15 -0700

    Added 4 arrays of 4 elements

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+# array of at least 4 strings
+anime_main_characters = ["Aoba Seragaki", "Izuku Midoriya", "Ash Ketchum", "Ciel Phantomhive", "Tanya von Degurechaff", "Naruto Uzumaki", "Boruto Uzumaki", "Luffy D Monkey"]
+
+# array of at least 4 integers
+my_favorite_numbers = [1, 3, 7, 9, 15, 21, 5, 0, 8]
+
+# array of at least 4 floats
+important_numbers = [3.14, 2.71, 1.61, 6.62, 6.02, 6.67, 1.38]
+
+# array of at least 4 booleans
+piercings = [true, false, true, false, false, true, true, false]
